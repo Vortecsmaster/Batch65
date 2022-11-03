d3f6891ea92b4987d6e1fec0aa3d07a02d630d09
Vortecsmaster <roberto.cerrud@vortecsla.com>
2022-11-03 10:12:15 -0500

    Simplified Validator

--- MathBounty/src/MathBountyO.hs
+++ MathBounty/src/MathBountyO.hs
@@ -11,7 +11,7 @@
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE DerivingStrategies  #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# LANGUAGE NumericUnderscores #-}
+
 
 module MathBountyO where
 
@@ -21,29 +21,23 @@ import           PlutusTx.Prelude hiding (pure, (<$>), Semigroup (..))
 import           Ledger                    
 import qualified Ledger.Constraints        as Constraints
 import qualified Ledger.Typed.Scripts      as Scripts
-import           Ledger.Ada                as Ada
-import           Ledger.TimeSlot
+
 
 --ON-CHAIN CODE
 
 data MathBountyDatum = MBD 
                       { mbdMath :: Integer
-                      , mbdDeadline :: POSIXTime
                       }
                       
 PlutusTx.makeIsDataIndexed ''MathBountyDatum [('MBD,0)]
 
 {-# INLINABLE mathBountyValidator #-}
 mathBountyValidator :: MathBountyDatum -> Integer -> ScriptContext -> Bool 
-mathBountyValidator datum redeemer sContext = traceIfFalse "Wrong X!" (mbdMath datum == redeemer*redeemer) &&
-                                              traceIfFalse "Deadline passed" deadlineReached
+mathBountyValidator datum redeemer sContext = traceIfFalse "Wrong X!" (mbdMath datum == redeemer*redeemer) 
     where
       info :: TxInfo
       info = scriptContextTxInfo sContext
-
-      deadlineReached :: Bool
-      deadlineReached =  (to $ mbdDeadline datum) `contains` (txInfoValidRange info)
-
+   
 data MathBounty
 instance Scripts.ValidatorTypes MathBounty where
     type instance RedeemerType MathBounty = Integer
